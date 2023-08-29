a5bd129d6dffa740daefb8f10df571c674083f75
Sujoy Nath <sujoy.csesust@gmail.com>
2023-08-29 23:36:53 +0600

    add mint, owner, transfer nft testing

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -4,7 +4,7 @@ const SUUPNFT = artifacts.require("SUUPNFT");
 module.exports = function(deployer) {
   const name = "SUUP NFT";
   const symbol = "SUUPNFT";
-  const admin = "0xf6a1a452AA3E1792600ADF1aA53f78A5Bc6708Ce"
+  const admin = "0x1cA32B885d69C983C8a94766d03dc119a09886Aa"
 
   deployer.deploy(Extra);
   deployer.link(Extra, SUUPNFT);
--- test/suup_token.js
+++ test/suup_token.js
@@ -23,18 +23,79 @@ contract('SUUPNFT', (accounts) => {
   const user_3 = accounts[7];
   const user_4 = accounts[8];
 
+  const uri = 'https://www.testurl.com/metadata';
+
   it('should add an admin', async() => {
-    const result = await suupNFTInstance.addAdmin(admin_1, { from: accounts[0]});
+    const result = await suupNFTInstance.addAdmin(admin_1, { from: admin});
     assert.isTrue(result.receipt.status, "Admin addition failed!");
   });
 
   it('should delete an admin', async() => {
-    const result = await suupNFTInstance.deleteAdmin(admin_1, { from: accounts[0]});
+    const result = await suupNFTInstance.deleteAdmin(admin_1, { from: admin});
     assert.isTrue(result.receipt.status, "Admin delete failed!");
   });
 
   it("should return admin list", async () => {
-    const adminList = await suupNFTInstance.adminLists({ from: accounts[0] });
+    const adminList = await suupNFTInstance.adminLists({ from: admin });
     assert.isArray(adminList, "Admin list is not an array");
+  });
+
+  it("should return current token id", async () => {
+    const totalSupply = await suupNFTInstance.totalSupply({ from: admin });
+    assert.equal(totalSupply.toString(), '0', "Invalid total supply");
+  });
+
+  it("should min nft and return token id", async () => {
+    
+    const mint = await suupNFTInstance.mint(user_1, uri, { from: admin});
+    assert.property(mint, 'tx', "Minting failed");
+    assert.property(mint, 'logs', "Minting failed");
+    assert.equal(mint.logs[0].args.tokenId, '1', "Minting failed");
+  });
+
+  it("should return owner of token", async () => {
+    const owner = await suupNFTInstance.ownerOf('1',{ from: admin});
+    assert.equal(owner, user_1, "Invalid owner of nft");
+  });
+
+  it("should return token id of token uri", async () => {
+    const tokenUri = await suupNFTInstance.tokenURI('1',{ from: admin});
+    assert.equal(tokenUri, uri, "Invalid token uri");
+  });
+
+  it("should approve a address for transfer token", async () => {
+    const result = await suupNFTInstance.approve(user_2, '1', { from: user_1});
+    assert.isTrue(result.receipt.status, "Approved fail");
+
+    const approved = await suupNFTInstance.getApproved('1', { from: user_1});
+    assert.equal(approved, user_2, "Invalid approved");
+    assert.notEqual(approved, user_3, "Invalid approved");
+  });
+
+  it("should approve a address for transfer token", async () => {
+    const result = await suupNFTInstance.approve(user_2, '1', { from: user_1});
+    assert.isTrue(result.receipt.status, "Approved fail");
+
+    const approved = await suupNFTInstance.getApproved('1', { from: user_1});
+    assert.equal(approved, user_2, "Invalid approved");
+    assert.notEqual(approved, user_3, "Invalid approved");
+  });
+
+  it("should set approved for all user", async () => {
+    const result = await suupNFTInstance.setApprovalForAll(user_2, true, { from: user_1});
+    assert.isTrue(result.receipt.status, "Approved fail");
+
+    const approved = await suupNFTInstance.getApproved('1', { from: user_1});
+    assert.equal(approved, user_2, "Invalid approved");
+    assert.notEqual(approved, user_4, "Invalid approved");
+  });
+
+  it("Transfer token from user_1 to user_2", async () => {
+    const result = await suupNFTInstance.safeTransferFrom(user_1, user_2, '1', { from: user_1});
+    assert.isTrue(result.receipt.status, "Transfer failed");
+
+    const owner = await suupNFTInstance.ownerOf('1', { from: admin});
+    assert.equal(owner, user_2, "Invalid transfer");
+    assert.notEqual(owner, user_1, "Invalid transfer");
   });
 });
